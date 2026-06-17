2ef55bc00ba27c01cabe14f9238a348800a7240b
daspk04 <dpratyush.k@gmail.com>
2026-06-17 13:51:18 -0500

    cli-proxy-api-plus: init at 7.1.68-2

---
+++ packages/cli-proxy-api-plus/package.nix
@@ -0,0 +1,56 @@
+{
+  lib,
+  buildGoModule,
+  go_1_26,
+  fetchFromGitHub,
+  unpinGoModVersionHook,
+  versionCheckHook,
+}:
+
+let
+  versionData = builtins.fromJSON (builtins.readFile ./hashes.json);
+  inherit (versionData) version hash vendorHash;
+in
+buildGoModule.override { go = go_1_26; } {
+  pname = "cli-proxy-api-plus";
+  inherit version vendorHash;
+
+  src = fetchFromGitHub {
+    owner = "kaitranntt";
+    repo = "CLIProxyAPIPlus";
+    rev = "v${version}";
+    inherit hash;
+  };
+
+  nativeBuildInputs = [ unpinGoModVersionHook ];
+
+  subPackages = [ "cmd/server" ];
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=${version}"
+    "-X main.Commit=nixpkgs"
+    "-X main.BuildDate=1970-01-01T00:00:00Z"
+  ];
+
+  postInstall = ''
+    mv $out/bin/server $out/bin/cli-proxy-api-plus
+  '';
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru.category = "Utilities";
+
+  meta = with lib; {
+    description = "Enhanced unified proxy providing compatible APIs for AI coding CLI tools";
+    homepage = "https://github.com/kaitranntt/CLIProxyAPIPlus";
+    changelog = "https://github.com/kaitranntt/CLIProxyAPIPlus/releases/tag/v${version}";
+    license = licenses.mit;
+    sourceProvenance = with lib.sourceTypes; [ fromSource ];
+    maintainers = with lib.maintainers; [ daspk04 ];
+    mainProgram = "cli-proxy-api-plus";
+    platforms = platforms.all;
+  };
+}
